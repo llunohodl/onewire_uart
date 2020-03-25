c49cbfb8b9f6484889076205c748168ee8997253
BykovIvan <llunohodl@gmail.com>
2020-03-25 20:32:59 +0500

    SPL UART  functions replaced by direct registers write

--- onewire_STM8L_hal.c
+++ onewire_STM8L_hal.c
@@ -3,36 +3,60 @@
 /* HAL for 1-Wire UART communications */
 
 #define LP_MODE 1 //Low power: 1 - Core off (WFI mode) while TX performed 
-//UART1_TX pin GPIOC pin 3
+//UART1_TX pin UART_PORT pin 3
 
 #define LP_DEBUG 1 //Use PC2 pin for debug
 
+#define UART_PORT GPIOC
+#define UART_PIN  (1<<3)
+#define DEBUG_PIN (1<<2)
+
 /* Set UART baundrate to 9600*/
 void OW_HAL_Speed_9600(){
-  GPIOC->ODR|=(1<<3);
-  GPIOC->DDR|=(1<<3);
+  UART_PORT->ODR|=UART_PIN;
+  UART_PORT->DDR|=UART_PIN;
   #if LP_DEBUG>0
-  GPIOC->ODR&=~(1<<2);
-  GPIOC->CR1|=(1<<2); //Push-pull
-  GPIOC->DDR|=(1<<2);
+  UART_PORT->ODR&=~DEBUG_PIN;
+  UART_PORT->CR1|=DEBUG_PIN; //Push-pull
+  UART_PORT->DDR|=DEBUG_PIN;
   #endif
+  #if 0
   USART_Init(USART1,9600,USART_WordLength_8b,USART_StopBits_1,USART_Parity_No,
              USART_Mode_Rx|USART_Mode_Tx);
   USART_HalfDuplexCmd(USART1, ENABLE);
+  #else
+  //9600
+  USART1->BRR2=0x00;
+  USART1->BRR1=0x1A;
+  //TX/RX enable
+  USART1->CR2=USART_CR2_TEN|USART_CR2_REN;
+  //Half-duplex
+  USART1->CR5=USART_CR5_HDSEL;
+  #endif
 }
 
 /* Set UART baundrate to 115200*/
 void OW_HAL_Speed_115200(){
-  GPIOC->ODR|=(1<<3);
-  GPIOC->DDR|=(1<<3);
+  UART_PORT->ODR|=UART_PIN;
+  UART_PORT->DDR|=UART_PIN;
   #if LP_DEBUG>0
-  GPIOC->ODR&=~(1<<2);
-  GPIOC->CR1|=(1<<2); //Push-pull
-  GPIOC->DDR|=(1<<2);
+  UART_PORT->ODR&=~DEBUG_PIN;
+  UART_PORT->CR1|=DEBUG_PIN; //Push-pull
+  UART_PORT->DDR|=DEBUG_PIN;
   #endif
+  #if 0
   USART_Init(USART1,115200,USART_WordLength_8b,USART_StopBits_1,USART_Parity_No,
              USART_Mode_Rx|USART_Mode_Tx);
   USART_HalfDuplexCmd(USART1, ENABLE);
+  #else
+  //115200
+  USART1->BRR2=0x02;
+  USART1->BRR1=0x02;
+  //TX/RX enable
+  USART1->CR2=USART_CR2_TEN|USART_CR2_REN;
+  //Half-duplex
+  USART1->CR5=USART_CR5_HDSEL;
+  #endif
 }
 
 static uint8_t RX_data=0;
@@ -48,7 +72,7 @@ uint8_t OW_HAL_SendRecive(uint8_t data){
   USART1->CR2 |= USART_CR2_RIEN;
   RX_flag=1;
   #if LP_DEBUG>0
-  GPIOC->ODR|=(1<<2);
+  UART_PORT->ODR|=DEBUG_PIN;
   #endif
   do{
     wfi(); //Wait for interrupt
@@ -69,7 +93,7 @@ INTERRUPT_HANDLER(USART1_RX_TIM5_CC_IRQHandler,28){
   #if LP_MODE>0
   if(USART1->SR&USART_SR_RXNE){
     #if LP_DEBUG>0
-    GPIOC->ODR&=~(1<<2);
+    UART_PORT->ODR&=~DEBUG_PIN;
     #endif
     RX_data=USART1->DR;
     RX_flag=0;
@@ -80,12 +104,12 @@ INTERRUPT_HANDLER(USART1_RX_TIM5_CC_IRQHandler,28){
 
 /* Set 1-Wire pin to communication mode */
 void OW_HAL_toUART(){
-  GPIOC->CR1&=~(1<<3); //Pseudo open drain
+  UART_PORT->CR1&=~UART_PIN; //Pseudo open drain
   CLK->PCKENR1|=CLK_PCKENR1_USART1; //Power on UART
 }
 
 /* Set 1-Wire pin to powering device mode */
 void OW_HAL_toPower(){
   CLK->PCKENR1&=~CLK_PCKENR1_USART1; //Power off UART
-  GPIOC->CR1|=(1<<3); //Push-pull
+  UART_PORT->CR1|=UART_PIN; //Push-pull
 }
